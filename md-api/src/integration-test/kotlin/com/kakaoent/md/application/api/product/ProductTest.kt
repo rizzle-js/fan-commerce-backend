d671b7c378feac9915f19313423cc85ca6ea5168
rizzle.js <rizzle.js@kakaoent.com>
2023-06-15 14:23:15 +0900

    통합 테스트 예시 및 규칙 README 작성

---
+++ md-api/src/integration-test/kotlin/com/kakaoent/md/application/api/product/ProductTest.kt
@@ -0,0 +1,32 @@
+package com.kakaoent.md.application.api.product
+
+import com.kakaoent.md.IntegrationTestSpec
+import com.kakaoent.md.application.product.ProductController.Companion.GET_PRODUCT_DETAIL
+import com.kakaoent.md.application.product.ProductDetailResponse
+import com.kakaoent.md.responseBody
+import io.kotest.matchers.shouldBe
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+class ProductTest(mockMvc: MockMvc) : IntegrationTestSpec({
+
+    feature("상품(Product)") {
+
+        scenario("상품을 조회하다") {
+
+            val productId = "1"
+
+            mockMvc.get(GET_PRODUCT_DETAIL, productId) {
+                contentType = APPLICATION_JSON
+            }
+                .andExpect {
+                    status { isOk() }
+                }
+                .responseBody<ProductDetailResponse>()
+                .also {
+                    it.productId shouldBe productId
+                }
+        }
+    }
+})
