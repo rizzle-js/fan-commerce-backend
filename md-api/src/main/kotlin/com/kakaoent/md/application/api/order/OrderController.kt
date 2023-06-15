d296571c983e50e79929a0ca80840338d412700c
joseph.ybh <joseph.ybh@kakaoent.com>
2023-06-15 16:11:15 +0900

    Mock 데이터 수정

--- md-api/src/main/kotlin/com/kakaoent/md/application/api/order/OrderController.kt
+++ md-api/src/main/kotlin/com/kakaoent/md/application/api/order/OrderController.kt
@@ -9,18 +9,13 @@ class OrderController {
     fun checkout(
         @RequestBody checkoutRequest: CheckoutRequest
     ): CheckoutResponse {
-        // 주문 ID를 생성하고, 결제를 진행하고, 체크아웃을 완료하고 결과를 반환하는
-        // 비즈니스 로직이 필요하며, 이는 상황에 따라 달라집니다.
-        val orderId = "4iat8YRCAFycmqVJydbEHH"
-        val totalPayment = checkoutRequest.orderItems.sumOf { it.price * it.quantity }
-        val checkedOutAt = Instant.now()
         return CheckoutResponse(
             memberKey = checkoutRequest.memberKey,
-            orderId = orderId,
+            orderId = "4iat8YRCAFycmqVJydbEHH",
             orderItems = checkoutRequest.orderItems,
             paymentMethod = checkoutRequest.paymentMethod,
-            totalPayment = totalPayment,
-            checkedOutAt = checkedOutAt,
+            totalPayment = checkoutRequest.orderItems.sumOf { it.price * it.quantity },
+            checkedOutAt = Instant.ofEpochSecond(1686641320L),
             deliveryInfo = checkoutRequest.deliveryInfo
         )
     }
