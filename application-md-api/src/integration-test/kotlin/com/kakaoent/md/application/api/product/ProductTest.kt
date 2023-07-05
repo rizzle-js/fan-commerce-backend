412bc10066955d5b6bbb19267a58fbc960b0d556
rizzle.js <rizzle.js@kakaoent.com>
2023-07-05 11:35:21 +0900

    상품 목록 조회 통합 테스트 구현

--- application-md-api/src/integration-test/kotlin/com/kakaoent/md/application/api/product/ProductTest.kt
+++ application-md-api/src/integration-test/kotlin/com/kakaoent/md/application/api/product/ProductTest.kt
@@ -1,30 +1,75 @@
 package com.kakaoent.md.application.api.product
 
 import com.kakaoent.md.IntegrationTestSpec
-import com.kakaoent.md.application.api.product.ProductController.Companion.GET_PRODUCT_DETAIL
+import com.kakaoent.md.application.api.product.ProductController.Companion.GET_PRODUCTS
+import com.kakaoent.md.domain.mall.MallProduct
+import com.kakaoent.md.domain.mall.MallProductRepository
+import com.kakaoent.md.domain.product.ProductRepository
+import com.kakaoent.md.fixture.product.mallProduct
+import com.kakaoent.md.fixture.product.product
 import com.kakaoent.md.responseBody
+import io.kotest.matchers.collections.shouldBeSortedWith
 import io.kotest.matchers.shouldBe
-import org.springframework.http.MediaType.APPLICATION_JSON
+import jakarta.persistence.EntityManager
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.DefaultTransactionDefinition
+import java.time.Instant
 
-class ProductTest(mockMvc: MockMvc) : IntegrationTestSpec({
+class ProductTest(
+    private val mockMvc: MockMvc,
+    private val productRepository: ProductRepository,
+    private val mallProductRepository: MallProductRepository,
+    private val platformTransactionManager: PlatformTransactionManager
+) : IntegrationTestSpec({
+
+    fun transactional(block: () -> Unit) {
+        val transaction = platformTransactionManager.getTransaction(DefaultTransactionDefinition())
+        try {
+            block()
+            platformTransactionManager.commit(transaction)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 
     feature("상품(Product)") {
 
-        scenario("상품을 조회하다") {
+        scenario("상품 목록을 조회하다") {
 
-            val productId = "1"
+            val mallId = "mallId"
 
-            mockMvc.get(GET_PRODUCT_DETAIL, productId) {
-                contentType = APPLICATION_JSON
+            val now = Instant.now()
+
+            transactional {
+                productRepository.save(product(productId = "1", displayedAt = now))
+                productRepository.save(product(productId = "2", displayedAt = now.minusSeconds(100)))
+                productRepository.save(product(productId = "3", displayedAt = now.minusSeconds(200)))
+                productRepository.save(product(productId = "4", displayedAt = now.minusSeconds(300)))
+                productRepository.save(product(productId = "5", displayedAt = now.minusSeconds(400)))
+
+                mallProductRepository.save(mallProduct(mallId = mallId, productId = "1"))
+                mallProductRepository.save(mallProduct(mallId = mallId, productId = "2"))
+                mallProductRepository.save(mallProduct(mallId = mallId, productId = "3"))
             }
-                .andExpect {
-                    status { isOk() }
-                }
-                .responseBody<ProductDetailResponse>()
+
+            val page = "0"
+            val size = "2"
+            val sort = "RECENT"
+
+            mockMvc.get(GET_PRODUCTS) {
+                param("mallId", mallId)
+                param("sort", sort)
+                param("page", page)
+                param("size", size)
+            }.andExpect {
+                status { isOk() }
+            }.responseBody<ProductsResponse>()
                 .also {
-                    it.productId shouldBe productId
+                    it.products.size shouldBe size.toInt()
+                    it.products shouldBeSortedWith compareByDescending { p ->  p.displayedAt }
+                    it.hasMore shouldBe true
                 }
         }
     }
